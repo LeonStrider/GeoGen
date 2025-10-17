a7930d8f07efb133851ba73e6bfe57aca51996ea
LeonStrider <LeonidXStrider@Gmail.com>
2025-10-17 16:01:27 +0300

    Update excludeiso.js
    Скорректирован список исключений

--- excludeiso.js
+++ excludeiso.js
@@ -1,7 +1,8 @@
 // ExcludeISO.js — подключать перед основным скриптом в HTML
 const excludeGroups = {
   "Нет исключений": [],
-  "Исключения для Galaxion": ["RU", "BY", "UA"],
-  "Исключения для Clickadu": ["AM","AZ","GE","KG","KZ","MD","TJ","TM","UZ","RU","UA","BY"]
+  "Исключения для Galaxion (RU BY UA)": ["RU", "BY", "UA"],
+  "Исключения для Clickadu (AM AZ GE KG KZ MD TJ TM UZ RU UA BY)": ["AM","AZ","GE","KG","KZ","MD","TJ","TM","UZ","RU","UA","BY"]
 };
+
 
