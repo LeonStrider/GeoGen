71aea4f40713e1005c23a7951acc9690abaf083e
LeonStrider <LeonidXStrider@Gmail.com>
2025-10-17 15:42:19 +0300

    Add files via upload

---
+++ excludeiso.js
@@ -0,0 +1,7 @@
+// ExcludeISO.js — подключать перед основным скриптом в HTML
+const excludeGroups = {
+  "Нет исключений": [],
+  "Исключения для Galaxion": ["RU", "BY", "UA"],
+  "Исключения для Clickadu": ["AM","AZ","GE","KG","KZ","MD","TJ","TM","UZ","RU","UA","BY"]
+};
+
